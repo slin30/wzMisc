83efee430fdb23ccafea794b17d66cca35cc4410
Wen Zhang <slin30@gmail.com>
2017-08-31 14:17:48 -0400

    Make last found value default for ctimefilt

--- R/ctime_filt.R
+++ R/ctime_filt.R
@@ -5,6 +5,7 @@
 #' @param pattern Optional regex passed to \code{list.files} pattern arg; defaults to \code{.*} (any).
 #' @param type One of "min" or "max" (quotes explicit) to denote if function should find
 #' earliest or latest file by creation time, respectively. Defaults to "max" (most recent file(s))
+#' @param only_last Should only the last file found be returned? Defaults to \code{TRUE}
 #' @param fmt \code{character} date format for ctime transformation. Defaults to \%Y-\%m-\%d
 #' @param print_limit Optional integer. How many found results should be printed?
 #' Defaults to 6. This ONLY affects display results, NOT returned results!
@@ -13,8 +14,9 @@
 #' This is most useful for filtering out multiple files with similar or identical file names within a single parent
 #' dir, regardless of nesting, by ctime as denoted by file.info.
 #'
-#' Beware if using directly for reading-in, as this function will list all files found according to input criteria;
-#' you should apply additional constraints for non-interactive use, e.g. specifying a more specific path and/or
+#' Beware if using directly for reading-in, as this function will list all files found according to input criteria,
+#' although it will also return only the last file found, by default. Nonetheless, you should
+#' apply additional constraints for non-interactive use, e.g. specifying a more specific path and/or
 #' pattern.
 #' @return
 #' A vector of files (full path names) that meet the type criteria (earliest or latest) and any other constraints,
@@ -24,7 +26,7 @@
 #' x <- list.files(R.home("doc"), full.names = TRUE)
 #' ctime_filt(x) #use defaults
 #' ctime_filt(x, pattern = ".html|.png", type = "max")
-ctime_filt <- function(x, pattern, type = c("max", "min"), fmt = "%Y-%m-%d", print_limit = 6L, ...) {
+ctime_filt <- function(x, pattern, type = c("max", "min"), only_last = TRUE, fmt = "%Y-%m-%d", print_limit = 6L, ...) {
 
   if(missing(type)) {
     message("Type arg missing; defaulting to max")
@@ -45,6 +47,21 @@ ctime_filt <- function(x, pattern, type = c("max", "min"), fmt = "%Y-%m-%d", pri
 
   out <- files[grepl(targ, finfo)]
 
+  if(length(out) == 0L) {
+    stop("No files found")
+  }
+  # the default option is also an early return
+  if(only_last) {
+    ret <- out[[length(out)]]
+    message(paste(note, "file timestamp is", targ,
+                  "corresponds to this file:\n",
+                  ret,
+                  "\n",
+                  "(file 1 of", length(out)), "found)")
+    return(invisible(ret))
+  }
+
+  # if you want all, though...
   message(paste(note, "file timestamp is", targ,
                 "and file count is:", length(out)
                 )
