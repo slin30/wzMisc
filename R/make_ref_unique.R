a072567b2069700c614bc85300c79e85977511c9
Wen Zhang <slin30@gmail.com>
2017-11-16 13:34:21 -0500

    Add make_ref_unique

---
+++ R/make_ref_unique.R
@@ -0,0 +1,108 @@
+#' Nake a lookup vector consisting of unique elements after applying fun
+#'
+#' For a vector x, apply fun and output result, named by unique x
+#'
+#' @param x (required) An input atomic vector
+#' @param dropNA Should \code{NA} be excluded, post-uniquification? Defaults to \code{TRUE}
+#' @param fun A character vector of length 1, denoting the function to apply to \code{x}
+#' @param ... Additional (ideally named) arguments to pass to \code{fun}, if applicable
+#'
+#' @details
+#' This is a convenient way to create lookup vectors, that can be used to transform the
+#' original vector via character subscripting, which can be more efficient (in some cases
+#' sigficantly) than applying the transformation on the input directly.
+#'
+#' @note
+#' \code{fun} should be passed as an explicitly named argument, i.e. \code{fun="gsub"} due to the
+#' \code{dropNA} order. Furthermore, it may be necessary to also explicitly pass \code{x} in the input
+#' within \code{...} if you wish to call a function (\code{fun}) that does not accept \code{x} as
+#' the first argument, AND where the argument name is something other than \code{x}.
+#'
+#' @return
+#' A named vector the length of \code{unique(x)}, or \code{length(unique(x[!is.na(x)]))}
+#' if \code{dropNA==TRUE}. Additionally, a message communicating the reduction in
+#' cardinality. If no reduction in cardinality was detected, i.e. \code{x} was already
+#' entirely unique, then a message stating that this function effectively has no benefit.
+#' @export
+#'
+#' @examples
+#' # an ideal scenario
+#' set.seed(10)
+#' vec_x <- rep(
+#'   replicate(1E4,
+#'             paste0(
+#'               paste(sample(LETTERS[1:4], 3, replace = TRUE), collapse = ""),
+#'               sample(100L:110L, 1)
+#'             )),
+#'   100
+#' )
+#'
+#' # using a lookup table to subscript
+#' system.time(
+#'   via_subscript <- make_ref_unique(vec_x, fun = "sub", pattern = "ABC", replacement = "")[vec_x]
+#' )
+#' # versus direct application
+#' system.time(via_direct <- sub(x = vec_x, pattern = "ABC", replacement = ""))
+#'
+#' # check
+#' identical(unname(via_subscript), via_direct)
+make_ref_unique <- function(x, dropNA = TRUE, fun, ...) {
+
+  if(!is.atomic(x)) {
+    stop("x must be an atomic vector")
+  }
+
+  if(all(is.na(x))) {
+    stop("x is all NA")
+  }
+
+  if(!is.character(fun) || length(fun) != 1) {
+    stop("fun must be a character vector of length 1")
+  }
+
+  u <- unique(x)
+
+  if(dropNA) {
+    u <- u[!is.na(u)]
+  }
+
+  # report how much you potentially save
+  reportmsg <- TRUE
+  xlen <- length(x)
+  lendelt <- xlen - length(u)
+  rem <- xlen - lendelt
+
+  if(lendelt == 0) {
+    message("Input is all unique")
+    reportmsg <- FALSE
+  }
+
+  v <- match.fun(fun)(u, ...)
+
+  dff <- round(lendelt/length(x) * 100, 1)
+  if(as.integer(dff) >= 99L) {
+    dff_msg <- paste0(">99")
+  } else if (as.integer(dff) <= 1L) {
+    dff_msg <- paste0("<1")
+  } else {
+    dff_msg <- paste0(dff)
+  }
+
+  msg <- paste0("Cardinality reduced by ",
+                paste0(
+                  dff_msg, "%"
+                ),
+                " (removed ",
+                lendelt, "/",
+                xlen,
+                ", keeping ",
+                rem,
+                ")"
+  )
+
+  if(reportmsg) {
+    message(msg)
+  }
+
+  setNames(v, nm = u)
+}
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 
 ## Miscellaneous functions package
-### Version 0.1.038
+### Version 0.1.039
 
 This is the initial release suitable for public consumption. 
 
