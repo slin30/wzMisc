02e6791b0370425bc3011a9fcd4a6272dfefaefc
Wen <slin30@gmail.com>
2018-05-04 12:57:14 -0400

    Try to fix Travis error on directory creation

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,7 +3,7 @@ library(wzMisc)
 
 #create if not exists testthat\testdata
 if(!dir.exists("./tests/testdata")) {
-  dir.create("./tests/testdata")
+  dir.create("./tests/testdata", recursive = TRUE)
 }
 
 test_check("wzMisc")
